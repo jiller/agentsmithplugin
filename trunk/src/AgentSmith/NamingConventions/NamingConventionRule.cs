ad84b0675a26c79a3d193e5f088d795fac133ba6
forever.zet <forever.zet@ec8eef40-f440-0410-a303-bf562d6d1485>
2008-03-31 15:29:47 +0000

    A couple of bug fixes

--- trunk/src/AgentSmith/NamingConventions/NamingConventionRule.cs
+++ trunk/src/AgentSmith/NamingConventions/NamingConventionRule.cs
@@ -355,9 +355,9 @@ namespace AgentSmith.NamingConventions
             if (name != originalName)
             {
                 forceError = true;
-                returnNames = new string[] {name};
+                returnNames = new string[] { name };
             }
-
+            
             if (!checkedObligatoryPrefixes)
             {
                 string[] prefixedNames = new string[_mustHavePrefixes.Length];
@@ -370,7 +370,11 @@ namespace AgentSmith.NamingConventions
             }
 
             if (!checkedObligatorySuffixes)
-            {
+            {                
+                if (returnNames.Length == 0)
+                {
+                    returnNames = new string[] { name };
+                }
                 string[] newNames = new string[_mustHaveSuffixes.Length * returnNames.Length];
                 for (int i = 0; i < _mustHaveSuffixes.Length; i++)
                 {
@@ -383,14 +387,7 @@ namespace AgentSmith.NamingConventions
                 forceError = true;
             }
 
-            if (forceError)
-            {
-                return returnNames;
-            }
-            else
-            {
-                return new string[0];
-            }
+            return forceError ? returnNames : new string[0];            
         }
     }
 }
