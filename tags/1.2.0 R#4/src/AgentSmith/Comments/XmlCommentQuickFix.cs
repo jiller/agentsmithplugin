65cbdc1e4f77403a5fafc95d2c876aaf2a45b1c5
forever.zet <forever.zet@ec8eef40-f440-0410-a303-bf562d6d1485>
2008-06-06 13:50:56 +0000

    made a copy

---
+++ tags/1.2.0 R#4/src/AgentSmith/Comments/XmlCommentQuickFix.cs
@@ -0,0 +1,93 @@
+using System;
+using JetBrains.Application;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Daemon;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.ReSharper.Psi.CSharp.Tree;
+using JetBrains.ReSharper.Psi.CSharp.Util;
+using JetBrains.ReSharper.Psi.Tree;
+using JetBrains.TextControl;
+using JetBrains.Util;
+
+namespace AgentSmith.Comments
+{
+    [QuickFix]
+    public class XmlCommentQuickFix : IQuickFix
+    {
+        private readonly IDeclaration _declaration;
+
+        public XmlCommentQuickFix(FixCommentSuggestion suggestion)
+        {
+            _declaration = suggestion.Element as IDeclaration;
+        }
+
+        #region IQuickFix Members
+
+        public bool IsAvailable(IUserDataHolder cache)
+        {
+            return true;
+        }
+
+        public IBulbItem[] Items
+        {
+            get { return new IBulbItem[] { new AddCommentBulbItem(_declaration) }; }
+        }
+
+        #endregion
+    }
+
+    internal class AddCommentBulbItem : IBulbItem
+    {
+        private readonly IDeclaration _declaration;
+
+        public AddCommentBulbItem(IDeclaration declaration)
+        {
+            _declaration = declaration;
+        }
+
+        #region IBulbItem Members
+
+        public void Execute(ISolution solution, ITextControl textControl)
+        {
+            PsiManager psiManager = PsiManager.GetInstance(solution);
+            if (psiManager.WaitForCaches("Agent Smith", "Cancel"))
+            {
+                using (CommandCookie.Create("QuickFix: " + Text))
+                {
+                    PsiManager manager = PsiManager.GetInstance(solution);
+                    manager.DoTransaction(delegate { ExecuteEx(solution, textControl); });
+                }
+            }
+        }
+
+        public string Text
+        {
+            get { return "Add comment stub."; }
+        }
+
+        #endregion
+
+        public void ExecuteEx(ISolution solution, ITextControl textControl)
+        {
+            IDocCommentBlockOwnerNode docCommentBlockOwnerNode =
+                XmlDocTemplateUtil.FindDocCommentOwner(_declaration as ITypeMemberDeclaration);
+
+            if (docCommentBlockOwnerNode != null)
+            {
+                int myCursorOffset;
+                string text = XmlDocTemplateUtil.GetDocTemplate(docCommentBlockOwnerNode, out myCursorOffset).Trim();
+                text = String.Format("///{0}\r\nclass Tmp {{}}", text.Replace("\n", "\n///"));
+
+                Logger.LogMessage(text);
+                Logger.LogMessage("Set comment.");
+
+                ICSharpTypeMemberDeclaration declaration =
+                    CSharpElementFactory.GetInstance(solution.SolutionProject).CreateTypeMemberDeclaration(text, new object[0]);
+                ICSharpTypeMemberDeclarationNode node = declaration.ToTreeNode();
+                docCommentBlockOwnerNode.SetDocCommentBlockNode(
+                    ((IDocCommentBlockOwnerNode)node).GetDocCommentBlockNode());
+            }
+        }
+    }
+}
---
+++ tags/1.2.0 R#4/src/AgentSmith/Options/CodeStyleSettings.cs
@@ -0,0 +1,166 @@
+using System;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+using AgentSmith.MemberMatch;
+using JetBrains.Application;
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Psi.CodeStyle;
+using JetBrains.ReSharper.Psi.Naming.DefaultNamingStyle;
+using JetBrains.Util;
+
+namespace AgentSmith.Options
+{
+    [Serializable]
+    [CodeStyleSettings("AgentSmithCSharpStyleSettings")]
+    public class CodeStyleSettings : IXmlExternalizable, ICloneable
+    {
+        private NamingConventionSettings _namingConventionSettings = new NamingConventionSettings();
+        private CommentsSettings _commentsSettings = new CommentsSettings();
+        
+        private CustomDictionaries _customDictionaries = new CustomDictionaries();
+        private string _stringsDictionary = "en-US";        
+        private string _identifierDictionary = "en-US";
+        private string _lastSelectedCustomDictionary = "en-US";
+
+        private Match[] _identifiersToSpellCheck;
+        private Match[] _identifiersNotToSpellCheck;
+        private bool _isJustImported;
+
+        public CodeStyleSettings()
+        {
+            _namingConventionSettings.LoadDefaults();
+            _commentsSettings = new CommentsSettings();
+            _commentsSettings.CommentMatch = new Match[] { new Match(Declaration.Any, AccessLevels.Public | AccessLevels.Protected | AccessLevels.ProtectedInternal), };
+            _identifiersToSpellCheck = new Match[] { new Match(Declaration.Any, AccessLevels.Public | AccessLevels.Protected | AccessLevels.ProtectedInternal) };
+            _isJustImported = true;
+        }
+
+
+        public CustomDictionaries CustomDictionaries
+        {
+            get { return _customDictionaries; }
+            set { _customDictionaries = value; }
+        }
+
+        public string StringsDictionary
+        {
+            get { return _stringsDictionary; }
+            set { _stringsDictionary = value; }
+        }
+
+        public string IdentifierDictionary
+        {
+            get { return _identifierDictionary; }
+            set { _identifierDictionary = value; }
+        }
+
+        public string LastSelectedCustomDictionary
+        {
+            get { return _lastSelectedCustomDictionary; }
+            set { _lastSelectedCustomDictionary = value; }
+        }
+
+        public CommentsSettings CommentsSettings
+        {
+            get { return _commentsSettings; }
+            set { _commentsSettings = value; }
+        }
+
+        public NamingConventionSettings NamingConventionSettings
+        {
+            get { return _namingConventionSettings; }
+            set { _namingConventionSettings = value; }
+        }
+
+        public Match[] IdentifiersToSpellCheck
+        {
+            get { return _identifiersToSpellCheck; }
+            set { _identifiersToSpellCheck = value; }
+        }
+
+        public Match[] IdentifiersNotToSpellCheck
+        {
+            get { return _identifiersNotToSpellCheck; }
+            set { _identifiersNotToSpellCheck = value; }
+        }
+
+        
+        [XmlIgnore]
+        public bool IsJustImported
+        {
+            get { return _isJustImported; }
+            set { _isJustImported = value;}
+        }
+
+        public static CodeStyleSettings GetInstance(ISolution solution)
+        {
+            JetBrains.ReSharper.Psi.CodeStyle.CodeStyleSettings settings = Shell.Instance.IsTestShell ? CodeStyleSettingsManager.Instance.CodeStyleSettings : SolutionCodeStyleSettings.GetInstance(solution).CodeStyleSettings;
+            return settings.Get<CodeStyleSettings>();
+        }
+
+        #region IXmlExternalizable implementation
+
+        #region ICloneable Members
+
+        public object Clone()
+        {
+            XmlDocument document = new XmlDocument();
+            XmlElement element = document.CreateElement("ID");
+            WriteToXml(element);
+            document.AppendChild(element);
+            CodeStyleSettings settings = new CodeStyleSettings();
+            settings.ReadFromXml(element);
+            return settings;
+        }
+
+        #endregion
+
+        #region IXmlExternalizable Members
+
+        public void ReadFromXml(XmlElement element)
+        {
+            if (element != null)
+            {
+                try
+                {
+                    XmlSerializer serializer = new XmlSerializer(GetType());
+                    XmlReader reader = XmlReader.Create(new StringReader(element.InnerXml));
+                    CodeStyleSettings settings = (CodeStyleSettings)serializer.Deserialize(reader);
+                    _namingConventionSettings = settings.NamingConventionSettings;
+                    _commentsSettings = settings.CommentsSettings;
+                    _customDictionaries = settings._customDictionaries;
+                    _stringsDictionary = settings._stringsDictionary;
+                    _identifierDictionary = settings._identifierDictionary;                    
+                    _lastSelectedCustomDictionary = settings._lastSelectedCustomDictionary;
+                    _identifiersToSpellCheck = settings._identifiersToSpellCheck;
+                    _identifiersNotToSpellCheck = settings._identifiersNotToSpellCheck;
+                    _isJustImported = false;
+                }
+                catch (Exception ex)
+                {
+                    Logger.LogException("Failed to load Agent Smith settings", ex);
+                }
+            }
+        }
+
+        public bool WriteToXml(XmlElement element)
+        {
+            element.SetAttribute("version", "0");
+            XmlSerializer serializer = new XmlSerializer(GetType());
+
+            StringWriter sWriter = new StringWriter();
+            XmlWriter writer = XmlWriter.Create(sWriter);
+
+            serializer.Serialize(writer, this);
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(sWriter.GetStringBuilder().ToString());
+            element.InnerXml = document.DocumentElement.OuterXml;
+            return true;
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
