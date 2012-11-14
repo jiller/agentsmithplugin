d51167515179c3ae1dce197bf3fec5bb7b8d921e
nebria80@gmail.com <nebria80@gmail.com@ec8eef40-f440-0410-a303-bf562d6d1485>
2012-11-14 08:48:04 +0000

    set version to 1.6.1.0

--- branches/R#7.1/src/AgentSmith/Properties/AssemblyInfo.cs
+++ branches/R#7.1/src/AgentSmith/Properties/AssemblyInfo.cs
@@ -14,8 +14,8 @@ using JetBrains.Application.PluginSupport;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: AssemblyVersion("1.6.0.1")]
-[assembly: AssemblyFileVersion("1.6.0.1")]
+[assembly: AssemblyVersion("1.6.1.0")]
+[assembly: AssemblyFileVersion("1.6.1.0")]
 
 [assembly: ActionsXml("AgentSmith.Actions.xml")]
 
